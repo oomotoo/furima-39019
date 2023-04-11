44094b2d56cb5face50c711edfbc02114b5c44bd
oomotoo <sota.kota.love@icloud.com>
2023-04-11 18:48:52 +0900

    ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    first_name            {'a'}
+    last_name             {'a'}
+    read_first_name       {'a'}
+    read_last_name        {'a'}
+    birthday              {'2003-05-13'}
+    password              {'000000'}
+    password_confirmation {password}
+  end
+end
