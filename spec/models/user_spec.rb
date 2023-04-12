3e3e126f3b2f4223cd23b28819577501dd896cda
oomotoo <sota.kota.love@icloud.com>
2023-04-12 16:17:52 +0900

    ユーザー管理機能

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -81,6 +81,26 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = "ｱｲｳｴｵ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = "ｱｲｳｴｵ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.first_name = "aiueo"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.first_name = "aiueo"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
     end
   end
 end
